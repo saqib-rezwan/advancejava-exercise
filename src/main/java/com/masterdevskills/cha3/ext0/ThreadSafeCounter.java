f8e1963cbbe5a3a6a47eb7387a0d07f71b37a3c5
saqib-rezwan <saqib_rezwan@hotmail.com>
2020-08-29 13:14:57 +0600

    Make functions of ThreadSafeCounter working

--- src/main/java/com/masterdevskills/cha3/ext0/ThreadSafeCounter.java
+++ src/main/java/com/masterdevskills/cha3/ext0/ThreadSafeCounter.java
@@ -6,8 +6,12 @@ package com.masterdevskills.cha3.ext0;
 public class ThreadSafeCounter {
 	private int count;
 
+	private Object object = new Object();
+
 	public void increment() {
-		count = count + 1;
+		synchronized (object){
+			count = count + 1;
+		}
 	}
 
 	public int getCount() {
