be44a8ca68b5005d4e8188162b63fc3c1adb6640
saqib-rezwan <saqib_rezwan@hotmail.com>
2020-08-29 20:01:51 +0600

    Failed experiment on thread

--- src/main/java/com/masterdevskills/cha3/ex1/ThreadPool.java
+++ src/main/java/com/masterdevskills/cha3/ex1/ThreadPool.java
@@ -1,31 +1,97 @@
 package com.masterdevskills.cha3.ex1;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 //TODO: Create a thread group field
 // Create a LinkedList field containing Runnable
 // Hint: Since LinkedList is not thread-safe, we need to synchronize it
 public class ThreadPool {
+
+	private ThreadGroup threadGroup = new ThreadGroup("ThreadGroup");
+	private Queue<Runnable> queue = new LinkedList<>();
+	private Object lock = new Object();
+	private int poolSize;
+
 	public ThreadPool(int poolSize) {
+		this.poolSize = poolSize;
+		for (int i = 0; i < poolSize; i++) {
+			Worker worker = new Worker(threadGroup, "Thread"+i);
+			worker.run();
+		}
 	}
 
-	private Runnable take() throws InterruptedException {
+	private synchronized Runnable take() throws InterruptedException {
 		//TODO if the LinkedList is empty, we wait
 		// remove the first job from the LinkedList and return it
-		throw new UnsupportedOperationException("not implemented");
+//		throw new UnsupportedOperationException("not implemented");
+
+//		synchronized (lock){
+//			while (queue.isEmpty()) {
+//				try {
+//					lock.wait();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			lock.notifyAll();
+//			return queue.poll();
+
+		while (queue.isEmpty()) {
+			try {
+				lock.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		lock.notifyAll();
+		return queue.poll();
+
 	}
 
 	public void submit(Runnable job) {
 		//TODO Add the job to the LinkedList and notifyAll
+
+//		System.out.println("submit invoked!");
+//		synchronized (lock){
+//			while (queue.size() == poolSize) {
+//				try {
+//					lock.wait();
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			queue.add(job);
+//			lock.notifyAll();
+//		}
+
+		while (queue.size() == poolSize) {
+			try {
+				queue.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		queue.add(job);
+		lock.notifyAll();
+
 	}
 
 	public int getRunQueueLength() {
 		//TODO return the length of the LinkedList
 		// remember to also synchronize!
-		throw new UnsupportedOperationException("not implemented");
+//		throw new UnsupportedOperationException("not implemented");
+
+		synchronized (lock){
+			return queue.size();
+		}
 	}
 
 	public void shutdown() {
 		//TODO this should call stop() on the ThreadGroup.
+
+		threadGroup.stop();
 	}
 
 	private class Worker extends Thread {
@@ -38,6 +104,15 @@ public class ThreadPool {
 			// we run in an infinite loop:
 			// remove the next job from the linked list using take()
 			// we then call the run() method on the job
+
+			while (true){
+				try {
+					take().run();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
 		}
 	}
 }
--- src/main/java/com/masterdevskills/cha3/ex3/ThreadPool.java
+++ src/main/java/com/masterdevskills/cha3/ex3/ThreadPool.java
@@ -1,10 +1,18 @@
 package com.masterdevskills.cha3.ex3;
 
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
 //TODO: Implement this thread pool using ExecutorService
 public class ThreadPool {
 
+	ExecutorService executorService;
+	ReentrantLock lock;
+
 	public ThreadPool(int poolSize) {
+		this.executorService = Executors.newFixedThreadPool(poolSize);
 	}
 
 	private Runnable take() throws InterruptedException {
@@ -12,7 +20,14 @@ public class ThreadPool {
 	}
 
 	public void submit(Runnable job) {
-
+		executorService.submit(() -> {
+			lock.lock();
+			try {
+				job.run();
+			} finally {
+				lock.unlock();
+			}
+		});
 	}
 
 	public int getRunQueueLength() {
@@ -21,7 +36,7 @@ public class ThreadPool {
 	}
 
 	public void shutdown() {
-
+		executorService.shutdown();
 	}
 
 }
